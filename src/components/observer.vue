e4b6735bd43a3cfb6d04937441f2781e83205acd
overlorddamygod <overlord.damygod@gmail.com>
2020-01-12 15:59:53 +0545

    master

---
+++ src/components/observer.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="observer">
+        <h2>See More</h2>
+        {{ valu}}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'observer',
+  data() {
+    return {
+      observer:null,
+      valu:''
+    }
+  },
+  mounted() {
+      this.observer= new IntersectionObserver(([entry]) => {
+          if (entry && entry.isIntersecting) {
+              this.$emit('intersect');
+          }
+
+      })
+
+      this.observer.observe(this.$el);
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.observer {
+    height: 10vh;
+
+}
+
+</style>
